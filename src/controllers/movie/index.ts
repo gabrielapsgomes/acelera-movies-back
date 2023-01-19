fdd49344557855cbb4c4dc7294410a47e537902f
gabrielapsgomes <gabrieladepaula8568@gmail.com>
2023-01-19 15:47:36 -0300

    Criar endpoint POST movie FEITO

--- src/controllers/movie/index.ts
+++ src/controllers/movie/index.ts
@@ -1,6 +1,8 @@
 import { getRepository } from "typeorm"
 import { Movie } from "@models/entity/Movie"
 import { Userlogin } from "@models/entity/UserLogin"
+import { response } from "express"
+import { error } from "console"
 
 export const movies = async (request, response) => {
   try {
@@ -20,13 +22,54 @@ export const userLogin = async (request, response) => {
     const user = await userRepository.findOne({ where: { email } })
 
     if (!user) {
-      return response.status(404).json({ auth: "false", message: "falha" })
+      return response.status(404).json({ auth: false, message: "falha" })
     }
     if (user.email === email && user.password === password) {
-      return response.status(200).json({ auth: "true", message: "sucesso" })
+      return response.status(200).json({ auth: true, message: "sucesso" })
     }
   } catch (error) {
     return response.status(500).json(error)
   }
 }
-//"id", "title", "releaseDate", "resume" "note", "image"
+
+export const postMovie = async (request, response) => {
+  try {
+    const movieRepository = getRepository(Movie)
+    const {
+      title,
+      gender,
+      classification,
+      subtitle,
+      image,
+      releaseDate,
+      director,
+      writer,
+      studio,
+      resume,
+      note,
+    } = request.body
+    let actors = request.body.actors.split(" ")
+    let awards = request.body.awards.split(" ")
+    const salvarMovies = movieRepository.create({
+      title,
+      gender,
+      classification,
+      subtitle,
+      image,
+      releaseDate,
+      director,
+      writer,
+      studio,
+      actors,
+      resume,
+      awards,
+      note,
+    })
+    console.log(salvarMovies)
+    await movieRepository.save(salvarMovies)
+
+    return response.status(200).json({ auth: true, message: "sucesso" })
+  } catch (error) {
+    return response.status(404).json({ auth: false, message: "falha" })
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { getTODO, itsWorks } from "@controllers/todo"
-import { movies, userLogin } from "@controllers/movie"
+import { movies, postMovie, userLogin } from "@controllers/movie"
 
 export const defineRoutes = (app) => {
   app.get("/", itsWorks)
   app.get("/todo", getTODO)
   app.get("/movie", movies)
   app.get("/login", userLogin)
+  app.post("/movie", postMovie)
 }
